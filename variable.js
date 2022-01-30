cec960d46b973409cf5c038b6a0526684eed1890
Azizul kabir lavlu <lavlu.bcse@gmail.com>
2022-01-31 02:45:19 +0600

    variable

---
+++ variable.js
@@ -0,0 +1,17 @@
+var varVariable = 'this is var';
+var varVariable = 'this is another var'
+// var function scope variable 
+// bar bar declire kora jay 
+// jekono jayga theke access kora jay 
+
+
+let letVariable = 'this is let';
+let letVariable = 'this is another let'; //eta kora jay na
+letVariable = 'this is another let'; //eta kora jay 
+// let block scope variable 
+// sudhu block scope theke access kora jay 
+// akbar diclire kore value change kora jay 
+
+const constVariable = 'this is const';
+// eta block scope variable 
+// akbar e declire kora jay and etar value change kora jay na 
