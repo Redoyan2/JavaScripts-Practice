c1732b5791c2f162b4d1d863eb724820983b60c4
Azizul kabir lavlu <lavlu.bcse@gmail.com>
2022-02-01 05:12:29 +0600

    13 er namota

---
+++ multiplicationTable.js
@@ -0,0 +1,9 @@
+// 13 er namota akta table akare dekhabe 
+
+
+function multiplicatonTable(num){
+    for (i=1; i<=num; i=i+1){
+        console.log(`${num} * ${i} = ${num*i}`);
+    }
+}
+let getMultiplication = multiplicatonTable(13);
