fa4608154da27e344e0ff3d8158131ea585810c4
Azizul kabir lavlu <lavlu.bcse@gmail.com>
2022-02-01 04:14:05 +0600

    hour to minute

---
+++ hourToMinute.js
@@ -0,0 +1,10 @@
+// Hour to minute calculation 
+
+
+
+function hourToMinute(hour){
+    let timeInMinute = hour*12;
+    return timeInMinute;
+}
+let getTimeInMinute = hourToMinute(2);
+console.log(getTimeInMinute);
