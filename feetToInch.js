7b842255d668f7a1fb92612c5272aa36881f67a0
Azizul kabir lavlu <lavlu.bcse@gmail.com>
2022-02-03 05:13:35 +0600

    feet to inch

---
+++ feetToInch.js
@@ -0,0 +1,13 @@
+// Give a value of feet and get it in inches 
+
+
+
+function feetToInch(feet){
+    let inch = feet*12;
+    return inch;
+}
+
+
+
+let getInch = feetToInch(5);
+console.log(getInch);
